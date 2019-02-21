f34fd464a70841cca64a706cc2bce7c40afc00e2
Jason Smith <mtbikerj@yahoo.com>
2019-02-21 18:47:47 +0000

    removal of custom container build.  Change to training using rasa/rasa_nlu:latest-full and HTTP API

--- bash/bare.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This bash script pulls the lasted image for rasa/rasa_nlu:latest-bare
-echo "Pulling rasa/rasa_nlu:latest-bare from Docker cloud.  This might take a few minutes..."
-docker image pull rasa/rasa_nlu:latest-bare
